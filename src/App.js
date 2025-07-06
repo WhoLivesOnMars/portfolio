1feb857d3ef4d01f5eac69b365d47b524982a5b8
Daria Khanina <khaninadaria@gmail.com>
2025-07-06 14:33:42 +0300

    fix: added basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="app">
-      <Router>
+      <Router basename="/portfolio">
         <ScrollToTop/>
         <Switch>
           <Route path="/" exact component={Main} />
